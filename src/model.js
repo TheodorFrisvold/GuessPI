c65cad804e86551e84b22f8ab7f1730a9de509d2
Flashforwarcl <asb.helgen@gmail.com>
2021-08-26 09:14:25 +0200

    update

--- src/model.js
+++ src/model.js
@@ -1,55 +1,57 @@
-//Model 
+//Model
 
 model = {
-    router: {
-        currentPage: 'homePage',
+  router: {
+    currentPage: "homePage",
+  },
+  main: {
+    playerName: "", //onChange check if player exists then update selectedPlayer and generate player object in db
+  },
+  globals: {
+    selectedPlayer: "", //populate with model.inputFields.main.playerName, manipulate with <- -> statspage
+    grabGameMode() {
+      let arr = model.gameModes.mode;
+      arr.forEach((el) => {
+        return console.log(el);
+      });
     },
-    main: {
-        playerName: ''                         //onChange check if player exists then update selectedPlayer and generate player object in db
+    grabPlayers() {
+      let index = model.data.players;
+      for (key in index) {
+        return console.log(index[key]);
+      }
     },
-    globals: {
-        selectedPlayer: '',                    //populate with model.inputFields.main.playerName, manipulte with <- -> statspage
-        grabGameMode() {
-            let arr = model.gameModes.mode
-            arr.forEach(el => {
-                return console.log(el)
-            });
-        },
-        grabPlayers() {
-            let index = model.data.players
-            for (key in index) {
-                return console.log(index[key])
-            }
-        },
-    },
-    gameInput: {
-    },
-    gameModes: {
-        mode: ['Practice', 'Normal']         //++ -- on <- ->  buttons
-    },
-    statistics: {
-        type: ['Top5', 'Graph'],
-    },
+  },
+  gameInput: {},
+  gameModes: {
+    mode: ["Practice", "Normal"], //++ -- on <- ->  buttons
+  },
+  statistics: {
+    type: ["Top5", "Graph"],
+  },
 
-    data: {
-        leaderboard: { List: [] },
-        players: [
-            {
-                playerId: 1,
-                playerName: 'Adrian',
-                gamesPlayed: {
-                    Normal: [{ date: '12/08/21', score: 18 }, { date: '12/08/21', score: 17 },],             // top 5 er fem første i liste
-                    Practice: []
-                },
-            },
-            {
-                playerId: 2,
-                playerName: 'Asbjorn',
-                gamesPlayed: {
-                    Normal: [],
-                    Practice: []
-                },
-            }
-        ]
-    }
+  data: {
+    leaderboard: { List: [] },
+    players: [
+      {
+        playerId: 1,
+        playerName: "Adrian",
+        gamesPlayed: {
+          Normal: [
+            { date: "12/08/21", score: 18 },
+            { date: "12/08/21", score: 17 },
+          ], // top 5 er fem første i liste
+          Practice: [],
+        },
+      },
+      {
+        playerId: 2,
+        playerName: "Asbjorn",
+        gamesPlayed: {
+          Normal: [],
+          Practice: [],
+        },
+      },
+    ],
+  },
 };
